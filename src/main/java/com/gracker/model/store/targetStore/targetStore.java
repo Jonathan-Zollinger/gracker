f476107d19852b876b300b80c83e102938a74a2e
jonathan <jonathan.zollinger@outlook.com>
2021-09-27 08:16:04 -0600

    starts targetStore

---
+++ src/main/java/com/gracker/model/store/targetStore/targetStore.java
@@ -0,0 +1,14 @@
+package com.gracker.model.store.targetStore;
+
+import com.gracker.model.store.Store;
+
+public class targetStore extends Store {
+    /**
+     * Constructor which assigns all available fields.
+     *
+     * @param homeURL current value for the homeURL field.
+     */
+    public targetStore(String homeURL) {
+        super(homeURL);
+    }
+}
